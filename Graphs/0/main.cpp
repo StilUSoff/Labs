67179c654dd25e63867b74e0be0517e69a5af442
Tugay Vadim <stilusoff@gmail.com>
2024-05-20 00:39:50 +0300

    del

--- Graphs/0/main.cpp
+++ Graphs/0/main.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-void generateFile(const string &file_name, const string &orig_text) {
+void Generate_File(const string &file_name, const string &orig_text) {
 	ofstream file;
 	file.open(file_name);
 	file << orig_text;
@@ -18,39 +18,42 @@ void generateFile(const string &file_name, const string &orig_text) {
 // Дерево 
 struct Node {
     char data;
-    int frequency;
+    int freq;
     Node* left;
     Node* right;
 
-    Node(char d, int freq) : data(d), frequency(freq), left(nullptr), right(nullptr) {}
+    Node(char d, int freq) : data(d), freq(freq), left(nullptr), right(nullptr) {}
     ~Node() {
         delete left;
         delete right;
     }
 };
+
 // Сортировка частот
-bool compareNodes(const Node* a, const Node* b) {
-    return a->frequency > b->frequency;
+bool Nodes_Comparing(const Node* a, const Node* b) {
+    return a->freq > b->freq;
 }
+
 // Построение дерева
-Node* buildHuffmanTree(vector<Node*> &nodes) {
+Node* Build_Tree(vector<Node*> &nodes) {
     while (nodes.size() > 1) {
-        sort(nodes.begin(), nodes.end(), compareNodes);
+        sort(nodes.begin(), nodes.end(), Nodes_Comparing);
 
-        Node* mergedNode = new Node('#', nodes[nodes.size() - 2]->frequency + nodes[nodes.size() - 1]->frequency);
-        mergedNode->left = nodes[nodes.size() - 2];
-        mergedNode->right = nodes[nodes.size() - 1];
+        Node* merged_node = new Node('#', nodes[nodes.size() - 2]->freq + nodes[nodes.size() - 1]->freq);
+        merged_node->left = nodes[nodes.size() - 2];
+        merged_node->right = nodes[nodes.size() - 1];
 
         nodes.pop_back();
         nodes.pop_back();
 
-        nodes.push_back(mergedNode);
+        nodes.push_back(merged_node);
     }
 
     return nodes[0];
 }
+
 // Коды 
-void generateCodes(Node* root, string code, map<char, string> &codes) {
+void Codes_Generation(Node* root, string code, map<char, string> &codes) {
     if (root == nullptr) {
         return;
     }
@@ -59,33 +62,35 @@ void generateCodes(Node* root, string code, map<char, string> &codes) {
         codes[root->data] = code;
     }
 
-    generateCodes(root->left, code + "0", codes);
-    generateCodes(root->right, code + "1", codes);
+    Codes_Generation(root->left, code + "0", codes);
+    Codes_Generation(root->right, code + "1", codes);
 }
+
 // Таблица кодов
-void printCodeTable(const map<char, string> &codes) {
+void Print_Table_of_Codes(const map<char, string> &codes) {
     cout << "Code Table:" << endl;
     for (const auto& pair : codes) {
         cout << pair.first << ": " << pair.second << endl;
     }
 }
+
 // Кодируем код
 string Fano_Encode(const string &text, const map<char, string> &codes) {
-    string encodedText = "";
+    string encoded_text = "";
 
     for (char c : text) {
-        encodedText += codes.at(c);
+        encoded_text += codes.at(c);
     }
 
-    return encodedText;
+    return encoded_text;
 }
 // Декодируем код
-string Fano_Decode(Node* root, const string &encodedText) {
+string Fano_Decode(Node* root, const string &encoded_text) {
     string decodedText = "";
 
     Node* current = root;
 
-    for (char bit : encodedText) {
+    for (char bit : encoded_text) {
         if (bit == '0') {
             current = current->left;
         }
@@ -140,7 +145,8 @@ string RLE_Decode(const string &orig_text) {
 int main() {
 
     setlocale(LC_ALL, "ru");
-    string alphabet = "MOPGSHmopgsh 0123456789^.";
+    string alphabet = "МОПГСЩмопгсщ 0123456789^.";
+    double encoding_price, compression_ratio;
     int n = 10000;
 
     string orig_text;
@@ -150,7 +156,7 @@ int main() {
     }
 
     string file_name = "txt/Original.txt";
-    generateFile(file_name, orig_text);
+    Generate_File(file_name, orig_text);
 
     // Создание алфавита и частот символов
     map<char, int> frequencies;
@@ -164,44 +170,58 @@ int main() {
         nodes.push_back(new Node(pair.first, pair.second));
     }
 
-    Node* root = buildHuffmanTree(nodes);
+    Node* root = Build_Tree(nodes);
 
     // Генерация кодов для символов
     map<char, string> codes;
-    generateCodes(root, "", codes);
+    Codes_Generation(root, "", codes);
 
     // Печать таблицы кода
-    printCodeTable(codes);
+    cout << endl;
+    Print_Table_of_Codes(codes);
 
-    // 1
+    // 1 (Fano)
+
+    cout << "\nFANO:" << endl;
 
     string fano = Fano_Encode(orig_text, codes);
     file_name = "txt/Solo_Fano_Encode.txt";
-    generateFile(file_name, fano);
+    Generate_File(file_name, fano);
 
     string fano_de = Fano_Decode(root, fano);
     file_name = "txt/Solo_Fano_Decoded.txt";
-    generateFile(file_name, fano_de);
+    Generate_File(file_name, fano_de);
 
     if (orig_text == fano_de) {
         cout << "FANO Success" << endl;
+        // Расчет цены кодирования
+        encoding_price = static_cast<double>(fano.size()) / orig_text.size();
+        cout << "Encoding Price: " << encoding_price << endl;
+        // Расчет коэффициента сжатия
+        compression_ratio = static_cast<double>(orig_text.size()) / (fano.size()/8);
+        cout << "Compression Ratio: " << compression_ratio << endl;
     }
     else {
         cout << "FANO Failed" << endl;
     }
 
-    // 2
+    // 2 (RLE)
+    
+    cout << "\nRLE:" << endl;
 
     string rle = RLE_Encode(orig_text);
     file_name = "txt/Solo_RLE_Encode.txt";
-    generateFile(file_name, fano);
+    Generate_File(file_name, rle);
 
     string rle_de = RLE_Decode(rle);
     file_name = "txt/Solo_RLE_Decoded.txt";
-    generateFile(file_name, fano_de);
+    Generate_File(file_name, rle_de);
 
     if (orig_text == rle_de) {
         cout << "RLE Success" << endl;
+        // Расчет коэффициента сжатия
+        compression_ratio = static_cast<double>(orig_text.size()) / rle.size();
+        cout << "Compression Ratio: " << compression_ratio << endl;
     }
     else {
         cout << "RLE Failed" << endl;
@@ -211,21 +231,26 @@ int main() {
 
     // RLE+Fano
 
+    cout << "\nRLE+FANO:" << endl;
+
     string rle_encoded = RLE_Encode(orig_text);
     file_name = "txt/R_F_RLE_Encode.txt";
-    generateFile(file_name, rle_encoded);
+    Generate_File(file_name, rle_encoded);
 
     string fano_rle_encoded = Fano_Encode(rle_encoded, codes);
     file_name = "txt/R_F_full_Encode.txt";
-    generateFile(file_name, fano_rle_encoded);
+    Generate_File(file_name, fano_rle_encoded);
 
     string fano_decoded = Fano_Decode(root, fano_rle_encoded);
     string fano_rle_decoded = RLE_Decode(fano_decoded);
     file_name = "txt/RLE_FANO_Decoded.txt";
-    generateFile(file_name, fano_rle_decoded);
+    Generate_File(file_name, fano_rle_decoded);
 
     if (orig_text == fano_rle_decoded) {
         cout << "RLE+FANO Success" << endl;
+        // Расчет коэффициента сжатия
+        compression_ratio = static_cast<double>(orig_text.size()) / fano_rle_encoded.size();
+        cout << "Compression Ratio: " << compression_ratio << endl;
     }
     else {
         cout << "RLE+FANO Failed" << endl;
@@ -233,25 +258,32 @@ int main() {
 
     // Fano+RLE
 
+    cout << "\nFANO+RLE:" << endl;
+
     string fano_encoded = Fano_Encode(orig_text, codes);
     file_name = "txt/F_R_Fano_Encode.txt";
-    generateFile(file_name, fano_encoded);
+    Generate_File(file_name, fano_encoded);
 
     string rle_fano_encoded = RLE_Encode(fano_encoded);
     file_name = "txt/F_R_full_Encode.txt";
-    generateFile(file_name, rle_fano_encoded);
+    Generate_File(file_name, rle_fano_encoded);
 
     string rle_decoded = RLE_Decode(rle_fano_encoded);
     string rle_fano_decoded = Fano_Decode(root, fano_encoded);
     file_name = "txt/FANO_RLE_Decoded.txt";
-    generateFile(file_name, rle_fano_decoded);
+    Generate_File(file_name, rle_fano_decoded);
 
     if (orig_text == rle_fano_decoded) {
         cout << "FANO+RLE Success" << endl;
+        // Расчет коэффициента сжатия
+        compression_ratio = static_cast<double>(orig_text.size()) / rle_fano_encoded.size();
+        cout << "Compression Ratio: " << compression_ratio << endl;
     }
     else {
         cout << "FANO+RLE Failed" << endl;
     }
+
+    cout << endl;
 
     return 0;
 }
